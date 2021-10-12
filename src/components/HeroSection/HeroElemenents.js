1c3a0488b5de2b506ea1a605e4ab7d430e25697c
tsmith90 <tsmith90@my.westga.edu>
2021-10-12 16:11:40 +0800

    Added video and playfair font.

--- src/components/HeroSection/HeroElemenents.js
+++ src/components/HeroSection/HeroElemenents.js
@@ -10,7 +10,25 @@ export const HeroContainer = styled.div`
     position: relative;
     z-index: 1;
 
-    /*Add :before styles*/
+    :before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: linear-gradient(
+            180deg,
+            rgba(0, 0, 0, 0.2) 0%,
+            rgba(0, 0, 0, 0.6) 100%,
+        ),
+        linear-gradient(
+            180deg,
+            rgba(0, 0, 0, 0.2) 0%,
+            transparent 100%,
+        );
+        z-index: 2;
+    }
 `;
 
 export const HeroBg = styled.div`
@@ -22,7 +40,7 @@ export const HeroBg = styled.div`
     width: 100;
     height: 100;
     overflow: hidden;
-`
+`;
 
 export const VideoBg = styled.video`
     width: 100%;
@@ -30,4 +48,44 @@ export const VideoBg = styled.video`
     -o-object-fit: cover;
     object-fit: cover;
     background: @232a34;
+`;
+
+export const HeroContent = styled.div`
+    z-index: 5;
+    max-width: 1200px;
+    position: absolute;
+    padding: 8px 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
+
+export const HeroH1 = styled.h1`
+    color: #fff;
+    font-size: 66px;
+    text-align: center;
+
+    @media screen and (max-width: 768px) {
+        font-size: 40px;
+    }
+
+    @media screen and (max-width: 480px) {
+        font-size: 32px;
+    }
+`
+export const HeroP = styled.p`
+    margin-top: 24px;
+    color: #fff;
+    font-size: 34px;
+    text-align: center;
+    max-width: 600px;
+
+    @media screen and (max-width: 768px) {
+        font-size: 24px;
+    }
+
+    @media screen and (max-width: 480px) {
+        font-size: 18px;
+    }
+`
+
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg } from './HeroElemenents';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP } from './HeroElemenents';
 
 const HeroSection = () => {
     return (
@@ -9,6 +9,10 @@ const HeroSection = () => {
                 <HeroBg>
                     <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
                 </HeroBg>
+                <HeroContent>
+                    <HeroH1>Todd Smith</HeroH1>
+                    <HeroP>Freelance Services For Tomorrow</HeroP>
+                </HeroContent>
             </HeroContainer>
         </>
     )
